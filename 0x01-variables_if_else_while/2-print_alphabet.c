4c8909ad0f1f832b24298fa48aaf9e756ec90fd6
Samachariak <Samachariak>
2023-03-18 21:19:50 -0700

    if else

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+		printf("\n");
+
+	return (0);
+}
